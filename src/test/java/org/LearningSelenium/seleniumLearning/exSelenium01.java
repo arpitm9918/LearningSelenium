517903bccdb0e03ad134ce3b709248cc6c8bd751
Arpit Mishra <arpitm041@gmail.com>
2024-10-13 09:39:08 +0530

    SeleniumLearning

---
+++ src/test/java/org/LearningSelenium/seleniumLearning/exSelenium01.java
@@ -0,0 +1,11 @@
+package org.LearningSelenium.seleniumLearning;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class exSelenium01 {
+    public static void main(String[] args) {
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+    }
+
+}
